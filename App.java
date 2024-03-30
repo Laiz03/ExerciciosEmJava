0c5103a06ca10789f1d8edf79f739c769149bfec
Laiz Nascimento <100091842+Laiz03@users.noreply.github.com>
2024-03-30 13:32:15 -0300

    Add files via upload

--- App.java
+++ App.java
@@ -1,16 +1,47 @@
 package com.example;
+import java.io.*;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        Conta c1 = new Conta ("Laíz", 123456);
-        c1.saldo = 1.000;
+        //cria o caminho do arquivo
+        File arquivoDestinoExercicio = new File("E:\\Atividades ADS\\DESENVOLVIMENTO DE SOFTWARE - CHICO\\exerciciolerescrever\\arquivoDestinoexercicio.txt");
 
-        System.out.println("Bem Vindo(a) " + c1.dono);
+        //cria o arquivo
+        try {
+            arquivoDestinoExercicio.createNewFile();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        //cria as variaveis
+
+        BufferedReader arquivoOrigemExercicio;
+        BufferedWriter arquivoDestinoExercicio2;
+
+        //cria o caminho
+        try {
+            arquivoDestinoExercicio2 = new BufferedWriter(new FileWriter
+            ("E:\\Atividades ADS\\DESENVOLVIMENTO DE SOFTWARE - CHICO\\exerciciolerescrever\\arquivoDestinoexercicio.txt"));
+            arquivoOrigemExercicio = new BufferedReader(new FileReader
+            ("E:\\Atividades ADS\\DESENVOLVIMENTO DE SOFTWARE - CHICO\\exerciciolerescrever\\arquivoOrigemexercicio.txt"));
+            String str;
+
+            //ler o arquivo e já escreve no arquivo novo
+            while ((str = arquivoOrigemExercicio.readLine()) != null){
+                arquivoDestinoExercicio2.write(str);
+                arquivoDestinoExercicio2.newLine(); 
+            }
+
+            arquivoDestinoExercicio2.close();
+            arquivoOrigemExercicio.close();
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+
+
     }
 }
